307e2a93d5e77349cc93fe17b6e765074b8be26b
Giga Amiridze <gigaamiridze@gmail.com>
2023-10-13 21:01:06 +0400

    Added fonts

--- interfaces/Theme.ts
+++ interfaces/Theme.ts
@@ -1,8 +1,14 @@
 export interface ITheme {
   colors: IColors;
+  fonts: IFonts;
 }
 
 interface IColors {
   white: string;
   black: string;
+}
+
+interface IFonts {
+  primary: string;
+  secondaty: string;
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,5 +2,9 @@ export const theme = {
   colors: {
     white: '#FFFFFF',
     black: '#000000',
-  }  
+  },
+  fonts: {
+    primary: '"Montserrat", sans-serif',
+    secondary: '"DM Serif Display", serif',
+  },
 }
