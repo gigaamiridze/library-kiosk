a853b0ad6711935b5ecf733b051da320c6d75967
Giga Amiridze <gigaamiridze@gmail.com>
2023-10-13 22:26:03 +0400

    Created Header and Footer components

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import { Header, Footer } from '../components';
 
 function Root() {
   return (
     <>
+      <Header />
       <Outlet />
+      <Footer />
     </>
   )
 }
