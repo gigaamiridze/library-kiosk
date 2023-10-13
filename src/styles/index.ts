5aebfbab769966ccd1cd3dfbbe52502641a79961
Giga Amiridze <gigaamiridze@gmail.com>
2023-10-14 01:12:30 +0400

    Created Container styled component

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,6 +1,7 @@
 // Shared components
 export { default as PreLoaderStyled } from './components/shared/PreLoaderStyled';
 export { default as HeadingStyled } from './components/shared/HeadingStyled';
+export { default as Container } from './components/shared/Container';
 export { default as FlexBox } from './components/shared/FlexBox';
 
 // Other
