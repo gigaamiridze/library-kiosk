16e85ab21f1a7c07c6b014fc49f4e35748efbc6f
Giga Amiridze <gigaamiridze@gmail.com>
2023-10-15 03:33:17 +0400

    Book selection page became protected

--- src/pages/BookSelection.tsx
+++ src/pages/BookSelection.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useLibraryContext } from '../contexts';
+import { PageRoutes } from '../constants';
+
 function BookSelection() {
+  const navigate = useNavigate();
+  const { libraryState } = useLibraryContext();
+
+  useEffect(() => {
+    if (!libraryState.selectedCategory) {
+      navigate(PageRoutes.BOOK_CATEGORIES);
+    }
+  }, []);
+
   return (
     <div>Book Selection</div>
   )
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -7,8 +7,8 @@ export const router = createBrowserRouter(
     <Route path={PageRoutes.MAIN_SERVICES} Component={Root}>
       <Route index Component={MainServices} />
       <Route path={PageRoutes.CONTACT} Component={Contact} />
+      <Route path={PageRoutes.BOOK_SELECTION} Component={BookSelection} />
       <Route path={PageRoutes.BOOK_CATEGORIES} Component={BookCategories} />
-      <Route path={`${PageRoutes.BOOK_SELECTION}/:categoryId`} Component={BookSelection} />
     </Route>
   )
 );
