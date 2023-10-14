ed604f47dfccf8c1cd8e9df3250c0d9ebe4daec8
Giga Amiridze <gigaamiridze@gmail.com>
2023-10-14 15:10:53 +0400

    Installed react query and added query client provider

--- src/constants/Api.ts
+++ src/constants/Api.ts
@@ -1,6 +1,6 @@
 const { VITE_API_KEY } = import.meta.env;
 
 export enum Api {
-  URL = 'https://www.googleapis.com/books/v1/volumes',
+  URL = 'https://www.googleapis.com/books/v1',
   KEY = VITE_API_KEY,
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
+const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
