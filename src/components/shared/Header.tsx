b491d50859da389c0caa5a797a39375bc77aa070
Giga Amiridze <gigaamiridze@gmail.com>
2023-10-17 01:08:41 +0400

    Deleted book selection navbar link

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,13 +1,10 @@
 import { Link } from 'react-router-dom';
 import { HeaderContainer, Container, FlexBox, Button } from '../../styles';
-import { useLibraryContext } from '../../contexts';
 import { PageRoutes } from '../../constants';
 import { Heading } from '../../components';
 import { images } from '../../assets';
 
 function Header() {
-  const { libraryState: { selectedCategory } } = useLibraryContext();
-
   return (
     <HeaderContainer>
       <Container>
@@ -32,17 +29,6 @@ function Header() {
                 <Link to={PageRoutes.BOOK_CATEGORIES}>
                   <Heading 
                     title='Book Categories'
-                    type='h6'
-                    fontWeight='500'
-                    hoverColor='purple'
-                    lineHeight={24}
-                  />
-                </Link>
-              </li>
-              <li>
-                <Link to={`${PageRoutes.BOOK_SELECTION}?category=${selectedCategory?.toLowerCase()}`}>
-                  <Heading 
-                    title='Book Selection'
                     type='h6'
                     fontWeight='500'
                     hoverColor='purple'
